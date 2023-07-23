aed67def91f23e1520f9bab8b1e30e7025597525
devSrabon <et.srabon@gmail.com>
2023-07-24 00:08:12 +0600

    partially complete/bayan

---
+++ src/controllers/bayan.controller.js
@@ -0,0 +1,35 @@
+const Bayan = require("../models.js/Bayans");
+
+exports.createBayan = async (req, res) => {
+  try {
+    const post = await Bayan.create({ ...req.body, user: req.user.userId });
+    res.status(201).json({
+      status: "success",
+      data: post,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "There was a server side error!",
+    });
+  }
+};
+
+exports.getBayan = async (req, res) => {
+  try {
+    const get = await Bayan.find({}).populate(
+      "user",
+      "-password -__v -posts -comments"
+    );
+
+    res.status(200).json({
+      status: "success",
+      data: get,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "There was a server side error!",
+    });
+  }
+};
--- src/router/bayan.router.js
+++ src/router/bayan.router.js
@@ -1,6 +1,9 @@
 const express = require("express");
 
 const verifyToken = require("../middleware/verifyToken");
+const { createBayan, getBayan } = require("../controllers/bayan.controller");
 const router = express.Router();
-
+router.use(verifyToken);
+router.post("/create", createBayan);
+router.get("/get", getBayan);
 module.exports = router;
