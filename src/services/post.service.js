dfe911155dfddc5bfb5eba0a85f2ecb4fb4e354e
devSrabon <et.srabon@gmail.com>
2023-07-30 20:53:24 +0600

    fix undefined errors

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -116,7 +116,7 @@ exports.getPostService = async (req) => {
   let count;
 
   try {
-    if (id) {
+    if (id && id !== "undefined") {
       const { following } = await User.findById(id);
       post = await Post.find({ user: { $in: following } })
         .populate({
